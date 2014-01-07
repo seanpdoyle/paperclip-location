1980bb56d8b36b7ae772fc5073686ad50a445806
Sean Doyle <sean.p.doyle24@gmail.com>
2014-01-06 22:07:14 -0500

    fixed require paths

--- lib/paperclip/location/processor.rb
+++ lib/paperclip/location/processor.rb
@@ -1,22 +1,28 @@
 require 'exifr'
 require 'paperclip'
-require 'paperclip/location/processor'
 
-class Paperclip::Location::Processor < Paperclip::Processor
-
-  delegate :location_locked?, to: :instance, allow_nil: true
-  delegate :instance, to: :attachment, allow_nil: true
-  delegate :gps, to: :exif
-
-  def make
-    if can_process?
-      instance.lat = gps.latitude
-      instance.lng = gps.longitude
+module Paperclip::Location
+  class Processor < Paperclip::Processor
+    def self.register!(name = :location)
+      Paperclip.configure do |c|
+        c.register_processor name, Paperclip::Location::Processor
+      end
     end
-    file
-  end
 
-  private
+    delegate :location_locked?, to: :instance, allow_nil: true
+    delegate :instance, to: :attachment, allow_nil: true
+    delegate :gps, to: :exif
+
+    def make
+      if can_process?
+        instance.lat = gps.latitude
+        instance.lng = gps.longitude
+      end
+      file
+    end
+
+    private
+
     def can_process?
       !location_locked? && gps.present? && instance.present?
     end
@@ -24,4 +30,5 @@ class Paperclip::Location::Processor < Paperclip::Processor
     def exif
       @exif ||= EXIFR::JPEG.new(file.path)
     end
+  end
 end
--- lib/paperclip/location/railtie.rb
+++ lib/paperclip/location/railtie.rb
@@ -1,7 +1,7 @@
-require 'paperclip/location/processor'
-
-class Paperclip::Location::Railtie < Rails::Railtie
-  initializer "paperclip-location.configure_rails_initialization" do
-    Paperclip::Location::Processor.register!
+module Paperclip::Location
+  class Railtie < Rails::Railtie
+    initializer "paperclip-location.configure_rails_initialization" do
+      Paperclip::Location::Processor.register!
+    end
   end
 end
